55d87146c6b7758ac26293a090d6dd10b6e12930
hussam8al <hussam7180@gmail.com>
2026-04-19 23:53:54 +0300

    Add index.php for Render detection

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// ملف أساسي لتعريف المشروع على منصة Render
+echo "Beauty Store Backend is running!";
+// يمكنك إعادة التوجيه للوحة التحكم إذا أردت
+// header('Location: admin/index.php');
+?>
